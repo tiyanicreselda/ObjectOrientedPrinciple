0f795845bdb709fd133041822176d7b865181c01
tiyanicreselda <tiyanicreselda050@gmail.com>
2022-12-05 08:51:14 +0200

    Add project files.

---
+++ ObjectOrientedPrinciple/People.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objectorientatedprinciples
+{
+    class People
+    {
+        public string Name;
+        public int Gender;
+        public string Race;
+        public string Age;
+
+
+        public People(string PersonName, string PersonGender, string PersonRace, int PersonAge)
+        {
+            Name = PersonName;
+            Gender = PersonAge;
+            Race = PersonRace;
+            Age = PersonGender;
+        }
+    }
+}
---
+++ ObjectOrientedPrinciple/Program.cs
@@ -0,0 +1,19 @@
+﻿using Objectorientatedprinciples;
+
+class Program
+{
+
+    static void Main(String[] args)
+    {
+        People Person1 = new People("Amukelani", "Female", "Black", 26);
+        People Person2 = new People("Creseldah", "Female", "Black", 19);
+        People Person3 = new People("Bongani", "Male", "Black", 08);
+
+        Console.WriteLine(Person1.Name + " " + Person1.Gender + " " + Person1.Race + " " + Person1.Age);
+        Console.WriteLine(Person2.Name + " " + Person2.Gender + " " + Person2.Race + " " + Person2.Age);
+        Console.WriteLine(Person3.Name + " " + Person3.Gender + " " + Person3.Race + " " + Person3.Age);
+
+    }
+
+
+}
